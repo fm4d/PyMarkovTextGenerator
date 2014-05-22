a2768deb4e5ec2382ccb9925821b972c021d0b9a
unkn0wn <unkn0wn@unkn0wn.com>
2014-05-22 15:52:26 +0200

    added setup.py and MANIFEST.in

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+url = 'https://github.com/FEE1DE4D/PyMarkovTextGenerator'
+setup(
+    name='PyMarkovTextGenerator',
+    version='1.0.0',
+    py_modules=['Markov'],
+
+    author='FEE1DE4D',
+    author_email='fee1de4d@gmail.com',
+
+    url=url,
+    description='Random text generator base on Markov chains.',
+    license='GPLv2+',
+
+    long_description="Documentation can be found here " + url,
+
+    classifiers=[
+        "License :: OSI Approved :: GNU General Public License v2 or later ("
+        "GPLv2+)",
+
+        "Programming Language :: Python :: 2.7",
+
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Text Processing :: General"
+    ],
+)
